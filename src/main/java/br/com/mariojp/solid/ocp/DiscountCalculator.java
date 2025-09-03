e4bd9ba8b2178392f5beed69bd563c74522738fe
Emilly Marques <emillymarquesx@gmail.com>
2025-09-03 09:03:21 -0300

    Alteração do DiscountCalculator

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,23 @@
 package br.com.mariojp.solid.ocp;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiscountCalculator {
+
+    public final Map<CustomerType, DiscountPolicy> policies;
+
+    public DiscountCalculator(){
+        policies = new HashMap<>();
+        policies.put(CustomerType.REGULAR, new DiscountPolicy.RegularPolicy());
+        policies.put(CustomerType.PREMIUM, new DiscountPolicy.PremiumPolicy());
+        policies.put(CustomerType.PARTNER, new DiscountPolicy.PartnerPolicy());
+    }
+
     public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
+        DiscountPolicy policy = policies.get(type);
+        if (policy == null){
+            throw new IllegalArgumentException(type);
         }
+        return policy.apply(amount);
     }
 }
